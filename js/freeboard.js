22f5099f19da790923502723f9ea080ae6c7ef59
Ghugan <ghugan.sivaperumal@tmrnd.com.my>
2018-03-05 11:07:52 +0800

    added switch widget

--- js/freeboard.js
+++ js/freeboard.js
@@ -534,7 +534,7 @@ function FreeboardModel(datasourcePlugins, widgetPlugins, freeboardUI)
 		}
 	}
 
-	this.saveDashboardClicked = function(_thisref, event){
+	this.saveDashboardClicked = function(){
 		var target = $(event.currentTarget);
 		var siblingsShown = target.data('siblings-shown') || false;
 		if(!siblingsShown){
